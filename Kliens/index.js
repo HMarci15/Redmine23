fc8bd767e00a38f5a45b52e3150fefd7aa2238ab
Horváth Marcell <horvathmarci03@gmail.com>
2024-04-03 21:38:25 +0200

    script szétszedése indexre és loginra

--- Kliens/index.js
+++ Kliens/index.js
@@ -1,12 +1,4 @@
-//Bejelentkezési funkció
-document.getElementById('loginForm').addEventListener('submit', function(event)
-{
-event.preventDefault()
-var username = document.getElementById('username').value;
-var password = document.getElementById('password').value;
-//szerveroldali hitelesítés 
-console.log('Bejelentkezési adatok:', username, password);
-});
+
 
 //Projektlista lekérdezése
 function getProjects(){
