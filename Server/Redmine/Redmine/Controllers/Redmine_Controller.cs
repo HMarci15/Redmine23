411422b9a1834b649dae81d97ffc06a86dcf0d17
kcsngr3 <164071481+kcsngr3@users.noreply.github.com>
2024-04-06 22:30:16 +0200

    api+web 2 lekerdezes

---
+++ Server/Redmine/Redmine/Controllers/Redmine_Controller.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Redmine.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Redmine.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProjectController : ControllerBase
+    {
+
+        /*        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginRequest loginRequest)
+        {
+            // Ellenőrzés, hogy a felhasználónév és jelszó megtalálható-e a listában
+            int matchCount = SampleData.Managers.Count(d => d.Email == loginRequest.Email && d.Password == loginRequest.Password);
+
+            return Ok(new { MatchCount = matchCount });
+        }   */
+
+        // Projekt listázása
+        [HttpGet]
+        public IEnumerable<object> GetProjects()
+        {
+            return SampleData.Projects.Select(project => new { project.ProjectId, project.Name }).ToList();
+        }
+
+        // Projekt feladatok listázása
+        [HttpGet("{projectId}/task")]
+        public IEnumerable<object> GetProjectTasks(int projectId)
+        {
+            var tasks = SampleData.Tasks.Where(task => task.ProjectId == projectId);
+            return tasks.Select(task => new { task.TaskId, task.Name }).ToList();
+        }
+    }
+}
