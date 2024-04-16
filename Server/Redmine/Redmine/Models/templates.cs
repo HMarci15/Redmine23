e0e9981db07ebbe52e5a5f7bdcc149387c8f5c32
kcsngr3 <testcsngr@gmail.com>
2024-04-16 18:07:25 +0200

    Model updates

---
+++ Server/Redmine/Redmine/Models/templates.cs
@@ -0,0 +1,11 @@
+﻿namespace Redmine.Models
+{
+    public class TaskModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Deadline { get; set; }
+        public int ManagerId { get; set; }
+        public int ProjectId { get; set; }
+    }
+}
