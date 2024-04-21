7f91c5e938d8eb628cf1197946005d8113ddfb34
kcsngr3 <testcsngr@gmail.com>
2024-04-21 21:40:13 +0200

    Fix Build

--- Server/Redmine/Redmine/obj/Debug/net8.0/Redmine.AssemblyInfo.cs
+++ Server/Redmine/Redmine/obj/Debug/net8.0/Redmine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Redmine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1830f133eabe6b10936c247ed1a520075002cc1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd3991df8ed8ee2018bd0f9d925d9ad45cced948")]
 [assembly: System.Reflection.AssemblyProductAttribute("Redmine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Redmine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
